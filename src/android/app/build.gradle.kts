085dad0c0e74a0d628579ac60233b595cedd90e3
Daniel Leite Lima <limaleite.daniel@gmail.com>
2024-05-06 22:02:22 +0200

    #31: Updates the version number for release.

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
 //        noinspection OldTargetApi
         targetSdk = 33
 
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
